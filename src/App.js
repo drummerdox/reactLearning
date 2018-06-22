8429538ce0645ad482ae344412d144ac40b23f0d
Alex <alexm@ikantam.com>
2018-06-23 01:12:52 +0300

    add filters

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const FILTER_ALL = 'all';
+const FILTER_ACTIVE = 'active';
+const FILTER_COMPLETED = 'completed';
+
 let todos = [
   {
     id: 1,
-    task: 'finish task',
-    isCompleted: false
+    task: 'wash dishesa',
+    isCompleted: false,
+    styleColor: 'isInCompleted',
   },
   {
     id: 2,
-    task: 'test task',
-    isCompleted: false
+    task: 'clean teeth',
+    isCompleted: false,
+    styleColor: 'isInCompleted',
   }
 ];
 
@@ -30,7 +36,8 @@ class App extends Component {
       if (item.id === todoItem.id) {
           answer = {
             ...item,
-            isCompleted: !item.isCompleted
+            isCompleted: !item.isCompleted,
+            styleColor: item.isCompleted == true ? 'isCompleted' : 'isInCompleted',
           }
       }
       return answer;
@@ -50,10 +57,9 @@ class App extends Component {
   }
 
   createTodo() {
-    let text = this.state.value;
     let newTodo = {
       id: this.state.todos.length + 1,
-      task: text,
+      task: this.state.value,
       isCompleted: false
     };
     
@@ -64,12 +70,25 @@ class App extends Component {
     })
   }
 
+  applyFilter(todos, filter) {
+    switch (todos) {
+        case FILTER_COMPLETED:
+            return todos.filter(todo => todos.isCompleted === true);
+
+        case FILTER_ACTIVE:
+            return todos.filter(item => todos.isCompleted !== true);
+
+        default:
+            return todos;
+    }
+  }
+
   render() {
     const todoList = (
       <ul>
         { todos.map((todo, key) => {
           return <li key={key}>
-          <button onClick={ (event) => {this.handleOnClick(todo)}}>{todo.task}</button>
+            <button className={todo.styleColor} onClick={ (event) => {this.handleOnClick(todo)}}>{todo.task}</button>
           </li>
         } ) }
       </ul>
@@ -78,7 +97,7 @@ class App extends Component {
       <div className="App">
       <div id="items">
       <h2>Список задач</h2>
-        <button onClick={this.createTodo.bind(this)}>Create new</button>
+        <button className={this.state.isCompleted} onClick={this.createTodo.bind(this)}>Create new</button>
         <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
         {todoList}
       </div>
