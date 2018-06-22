c2c76c6951ede14f7a8ce0ba78dad8f87e31cace
Alexander <pidginbox@gmail.com>
2018-06-22 17:30:04 +0300

    add status modification

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 const todos = [
-{
-  task: 'finish task',
-  isCompleted: false
-},
-{
-  task: 'test task',
-  isCompleted: false
-}
+  {
+    id: 1,
+    task: 'finish task',
+    isCompleted: false
+  },
+  {
+    id: 2,
+    task: 'test task',
+    isCompleted: false
+  }
 ];
 
 class TodoList extends Component {
@@ -37,17 +39,39 @@ class App extends Component {
       todos: todos
     }
   }
+
+  handleOnClick = (todoItem) => {
+    this.setState({
+      todos: this.state.todos.map((item) => {
+        let answer = item;
+        if (item.id === todoItem.id) {
+            answer = {
+              ...item,
+              isCompleted: !item.isCompleted
+            }
+        }
+        return answer;
+      })
+    }
+   );
+  }
+
   render() {
+    const todoList = (
+      <ul>
+        { todos.map((todo, key) => {
+          console.log(todo);
+          return <li key={key}><button onClick={(event) => {this.handleOnClick(todo)}}>{todo.task}</button></li>
+        } ) }
+      </ul>
+    );
+    console.log(todoList);
     return (
-      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to my TODO list</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="items">
+      <h2>Список телефонов</h2>
+      {todoList}
+      </div>
       </div>
     );
   }
